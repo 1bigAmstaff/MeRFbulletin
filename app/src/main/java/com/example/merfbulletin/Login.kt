706aca258483b664b82d8ab8497a919772ab983c
Oliver-Cho <ocho@wesleyan.edu>
2024-12-26 14:21:31 -0500

    added navigation between login and registration screens

--- app/src/main/java/com/example/merfbulletin/Login.kt
+++ app/src/main/java/com/example/merfbulletin/Login.kt
@@ -1,20 +1,60 @@
 package com.example.merfbulletin
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+    private lateinit var editTextEmail: TextInputEditText
+    private lateinit var editTextPassword: TextInputEditText
+    private lateinit var buttonLogin: Button
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var progressBar: ProgressBar
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        mAuth = FirebaseAuth.getInstance()
+        editTextEmail = findViewById(R.id.editTextEmail)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        buttonLogin = findViewById(R.id.btn_login)
+        progressBar = findViewById(R.id.progressBar)
+        textView = findViewById(R.id.registerNow)
+
+        textView.setOnClickListener {
+            val intent = Intent(applicationContext, Register::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        buttonLogin.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(this, "Enter email", Toast.LENGTH_SHORT).show()
+                progressBar.visibility = View.GONE
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show()
+                progressBar.visibility = View.GONE
+                return@setOnClickListener
+            }
+
+
         }
     }
 }
--- app/src/main/java/com/example/merfbulletin/Register.kt
+++ app/src/main/java/com/example/merfbulletin/Register.kt
@@ -1,20 +1,86 @@
 package com.example.merfbulletin
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class Register : AppCompatActivity() {
+
+    private lateinit var editTextEmail: TextInputEditText
+    private lateinit var editTextPassword: TextInputEditText
+    private lateinit var buttonReg: Button
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var progressBar: ProgressBar
+    private lateinit var textView: TextView
+
+    companion object {
+        private const val TAG = "RegisterActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        mAuth = FirebaseAuth.getInstance()
+        editTextEmail = findViewById(R.id.editTextEmail)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        buttonReg = findViewById(R.id.btn_register)
+        progressBar = findViewById(R.id.progressBar)
+        textView = findViewById(R.id.loginNow)
+        textView.setOnClickListener {
+            val intent = Intent(applicationContext, Login::class.java)
+            startActivity(intent)
+            finish()
         }
+
+        buttonReg.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(this, "Enter email", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        progressBar.visibility = View.GONE
+                        Toast.makeText(
+                            this@Register,
+                            "Account created.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            this@Register,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        // Update UI based on the user object
     }
 }
