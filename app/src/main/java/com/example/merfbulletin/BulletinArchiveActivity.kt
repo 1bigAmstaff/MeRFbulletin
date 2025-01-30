a514bdba9f8fcdb39f9626f301e9d2b206dcbef0
Oliver-Cho <ocho@wesleyan.edu>
2025-01-30 15:36:38 -0500

    made bulletin archive button lead to a new activity to be populated

--- app/src/main/java/com/example/merfbulletin/BulletinArchiveActivity.kt
+++ app/src/main/java/com/example/merfbulletin/BulletinArchiveActivity.kt
@@ -1,12 +1,32 @@
 package com.example.merfbulletin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class BulletinArchiveActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bulletin_archive)
+
+        auth = FirebaseAuth.getInstance()
+
+        val bulletinTextView: TextView = findViewById(R.id.bulletin_archive_text)
+        val bulletinArray = BulletinArray()
+        bulletinTextView.text = bulletinArray.getCurr()
+
+        val btnLogout: Button = findViewById(R.id.btn_logout)
+        btnLogout.setOnClickListener {
+            auth.signOut()
+            val intent = Intent(applicationContext, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
