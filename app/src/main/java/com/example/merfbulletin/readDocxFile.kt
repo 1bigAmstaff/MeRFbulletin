802ab67bc7647160b8e0e1d508ed36815348599e
Oliver-Cho <ocho@wesleyan.edu>
2025-02-03 21:13:18 -0500

    added scrolling to this week's bulletin activity view

--- app/src/main/java/com/example/merfbulletin/readDocxFile.kt
+++ app/src/main/java/com/example/merfbulletin/readDocxFile.kt
@@ -5,10 +5,13 @@ import java.io.InputStream
 
 fun readDocxFile(inputStream: InputStream): String {
     val document = XWPFDocument(inputStream)
+    println("document: $document")
     val paragraphs = document.paragraphs
+    println("paragraphs: $paragraphs")
     val stringBuilder = StringBuilder()
     for (paragraph in paragraphs) {
         stringBuilder.append(paragraph.text).append("\n")
     }
+    println("stringBuilder: $stringBuilder")
     return stringBuilder.toString()
 }
